28734b650981076e46f7e792a7a918bacf19a793
Maston <MastonDZN@users.noreply.github.com>
2023-04-08 03:33:27 +0300

    fix lint breaking

--- www/.eslintrc.cjs
+++ www/.eslintrc.cjs
@@ -3,7 +3,7 @@ const { defineConfig } = require('eslint-define-config');
 module.exports = defineConfig({
     root: true,
     extends: [require.resolve('@cloudflare-example/eslint-config-custom')],
-    ignorePatterns: ['node_modules', 'dist', 'coverage', 'build'],
+    ignorePatterns: ['node_modules', 'dist', 'coverage', 'build', 'vite.config.ts'],
     parserOptions: {
         project: './tsconfig.json',
     },
--- www/src/main.tsx
+++ www/src/main.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app';
@@ -12,11 +12,11 @@ const theme = extendTheme({
 });
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
-    <React.StrictMode>
+    <StrictMode>
         <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={theme}>
                 <App />
             </ChakraProvider>
         </QueryClientProvider>
-    </React.StrictMode>
+    </StrictMode>
 );
